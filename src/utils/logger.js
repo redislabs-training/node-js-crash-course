31f88348419c88403290fff8c776d61583799d82
Simon Prickett <simon@redislabs.com>
2021-02-24 21:22:10 +0000

    Refactored where logging config lives.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const config = require('better-config');
 
 const logger = winston.createLogger({
-  level: config.get('application.logLevel'),
+  level: config.get('logLevel'),
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(
