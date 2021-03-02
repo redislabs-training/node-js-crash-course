24197a9bdd292d4b15339583868aa4cee7081e23
Simon Prickett <simon@redislabs.com>
2021-03-02 16:06:41 +0000

    Adds timestamp validation.

--- src/routes/checkin_routes.js
+++ src/routes/checkin_routes.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
+const { param } = require('express-validator');
 const apiErrorReporter = require('../utils/apierrorreporter');
+
+const timestampValidator = (value, { req }) => {
+  const startTime = parseInt(req.params.startTime, 10);
+  const endTime = parseInt(req.params.endTime, 10);
+
+  if (startTime > endTime) {
+    throw new Error('startTime must be less than or equal to endTime.');
+  }
+
+  return true;
+};
 
 router.get(
   '/checkins/:startTime/:endTime',
+  [
+    param('startTime').isInt({ min: 0 }).custom(timestampValidator),
+    param('endTime').isInt({ min: 0 }).custom(timestampValidator),
+    apiErrorReporter,
+  ],
   async (req, res, next) => res.status(200).json({ status: 'TODO' }),
 );
 
